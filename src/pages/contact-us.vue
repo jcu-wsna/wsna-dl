09b070f87b715969ffbf4f64b8cdfbbf7d3b2aa1
Pauline Lawrey <pauline.lawrey@jcu.edu.au>
2024-05-30 14:55:45 +1000

    Updated Contact Us content to new text from Yvette

--- src/pages/contact-us.vue
+++ src/pages/contact-us.vue
@@ -3,7 +3,11 @@
     <div class="q-pa-lg q-gutter-lg">
       <q-card flat>
         <q-card-section>
-          We'd love to hear from you please contact us at
+          The Northern Australia Resources Digital Library welcomes contributions from individuals,
+          organizations, and researchers. Any data or research relevant to the region's water resources
+          and ecosystems is encouraged for submission, and all contributions will be thoughtfully considered
+          for inclusion in the repository.
+          Please send your contributions to the email address below.
           <a href="mailto:tropwater@jcu.edu.au">tropwater@jcu.edu.au</a>.
         </q-card-section>
       </q-card>
